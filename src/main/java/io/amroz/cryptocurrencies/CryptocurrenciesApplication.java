4b993d11b7e0bbb0f9aac1f531e8ddc26e5a6c71
Adam Mróz <adam.michal.mroz@gmail.com>
2020-06-02 08:18:34 +0200

    Project init

---
+++ src/main/java/io/amroz/cryptocurrencies/CryptocurrenciesApplication.java
@@ -0,0 +1,13 @@
+package io.amroz.cryptocurrencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrenciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptocurrenciesApplication.class, args);
+	}
+
+}
